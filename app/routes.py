d7964369737d3b9810b521a71bbb8352a325569f
ILHAMDEANABDILLAH <deanabdillah@gmail.com>
2022-06-11 01:50:06 +0700

    perubahan bersar

--- app/routes.py
+++ app/routes.py
@@ -2,5 +2,14 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def f_index():
+    return render_template('home.html')
+
+@app.route('/admin')
+@app.route('/inputUser')
+def f_admin():
+    return render_template('admFormUsr.html')
+
+@app.route('/tabelUsers')
+def f_tabelUsers():
+    return render_template('admTblUsrs.html')
