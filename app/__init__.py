a5950388c1f39e73fae945f68bdc0f3f4256b9cc
ILHAMDEANABDILLAH <deanabdillah@gmail.com>
2022-06-10 07:23:04 +0700

    tambah dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 from app import routes
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,5 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template('index.html')
